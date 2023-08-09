f2cdf4e76f6d42fe1f0e132b6a7ca77c353ac8b6
vik24rus <vik24rus@gmail.com>
2023-08-09 23:30:39 +0700

    .

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,24 @@
+# -*- coding: utf-8 -*-
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
-app.run( debug=True)
+    user = {'username': 'Эльдар Рязанов'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
